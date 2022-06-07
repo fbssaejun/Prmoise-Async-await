5f78a250f66e4204e617db49bdbe697f8de23174
Anthony Kim <fbssaejun@gmail.com>
2022-06-06 23:32:45 -0400

    create function that queues an api and awaits the result, converts it into json and logs it to the console

--- task3.js
+++ task3.js
@@ -1,2 +1,13 @@
 // Task 3: using https://restcountries.eu/ API,
 // get country where alpha3Code = col
+
+const fetchCountry = async () => {
+  // Get the result of the API queue
+  const res = await fetch('https://restcountries.eu/rest/v2/alpha/col');
+  // Convert the returned data into json format
+  const country = await rest.json();
+  // Log out the reulst
+  console.log(country);
+}
+
+fetchCountry();
